e91a9670ad1354a79100c52b585d4156d7a926fe
Scott MacGregor <smacgregor@zynga.com>
2016-03-12 14:21:34 -0800

    Issue #69 - add an image gallery view for swiping through images for the pet

--- app/src/main/java/com/codepath/apps/critterfinder/fragments/PetDetailsFragment.java
+++ app/src/main/java/com/codepath/apps/critterfinder/fragments/PetDetailsFragment.java
@@ -2,17 +2,20 @@ package com.codepath.apps.critterfinder.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.apps.critterfinder.R;
+import com.codepath.apps.critterfinder.adapters.ImageGalleryAdapter;
 import com.codepath.apps.critterfinder.models.PetModel;
-import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,7 +30,8 @@ public class PetDetailsFragment extends Fragment {
     @Bind(R.id.text_pet_description) TextView mPetDescription;
     @Bind(R.id.text_pet_gender) TextView mPetGender;
     @Bind(R.id.text_pet_name) TextView mPetName;
-    @Bind(R.id.image_pet) ImageView mPetImage;
+    @Bind(R.id.pet_image_gallery) ViewPager mImageGallery;
+    private ImageGalleryAdapter mImageGalleryAdapter;
 
     private PetModel mPet;
 
@@ -50,16 +54,24 @@ public class PetDetailsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_pet_details, container, false);
         ButterKnife.bind(this, view);
         setupPetDetailsView();
+        setupPetImageGallery();
         return view;
     }
 
     private void setupPetDetailsView() {
         mPetName.setText(mPet.getName());
         mPetGender.setText(mPet.getSexFullName());
-        Picasso.with(mPetImage.getContext()).
-                load(mPet.getImageUrl()).
-                //resize(DeviceDimensionsHelper.getDisplayWidth(getContext()), 0).
-                into(mPetImage);
         mPetDescription.setText(mPet.getDescription());
+    }
+
+    private void setupPetImageGallery() {
+        // TODO - once our pet models has an array of pet images we can use that
+        List<String> images = new ArrayList<>();
+        images.add(mPet.getImageUrl());
+        images.add("https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12729479_184511348589610_36145994_n.jpg");
+        images.add("https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12750125_1667429310176636_4996515_n.jpg");
+        images.add("https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12071062_736784449785114_1231793951_n.jpg");
+        mImageGalleryAdapter = new ImageGalleryAdapter(getContext(), images);
+        mImageGallery.setAdapter(mImageGalleryAdapter);
     }
 }
